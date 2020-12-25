5dc7ff88865f047599d1bf611c89d83cd217efe6
Tim Hitchins <tim.x.hitchins@gmail.com>
2020-12-24 18:14:26 -0800

    finish styles for nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,17 +12,23 @@ const hashLinks: string[] = ["About Me", "Portfolio", "CV", "Contact"];
 
 function Navbar({ isOpen }: NavProps) {
   const [activeLink, setActiveLink] = useState<null | string>(null);
-  const [drawerIsOpen, toggleNavDrawer] = useState<boolean>(false);
-  const isMobile = useMediaQuery({ query: `(max-width: ${styles.mobileBreak}` });
+  const [isDrawerOpen, toggleNavDrawer] = useState<boolean>(false);
+  const isMobile = useMediaQuery({
+    query: `(max-width: ${styles.mobileBreak})`.replace(/"/gi, ""), // turn into function
+  });
   const location = useLocation();
+
+  console.log("isDrawerOpen: ", isDrawerOpen);
+  console.log("isMobile: ", isMobile);
 
   return (
     <nav className="nav-outer-container">
       <div
-        className="nav-inner-container"
-        style={isOpen ? { height: "auto" } : { height: "56px" }}
+        className={
+          isMobile ? "nav-inner-container-mobile" : "nav-inner-container"
+        }
       >
-        <div>
+        <div className="nav-logo">
           <NavLink to="/" exact>
             <img
               src="https://timsportfolio2020.s3-us-west-2.amazonaws.com/horizontal-dark-color-270x72.png"
@@ -31,11 +37,19 @@ function Navbar({ isOpen }: NavProps) {
           </NavLink>
         </div>
         <div></div>
+
         {hashLinks.map((link) => {
           return (
             <div
               key={link}
               className={activeLink === link ? "nav-link-active" : "nav-link"}
+              style={
+                isDrawerOpen && isMobile
+                  ? { display: "block" }
+                  : !isDrawerOpen && isMobile
+                  ? { display: "none" }
+                  : { display: "block" }
+              }
             >
               <HashLink
                 to={`#${link.replace(" ", "-").toLowerCase()}`}
@@ -46,15 +60,14 @@ function Navbar({ isOpen }: NavProps) {
             </div>
           );
         })}
-
-        <div
-          className="hamburger-button"
-          onClick={() => {
-            return null;
-          }}
-        >
-          &#x2630;
-        </div>
+      </div>
+      <div
+        className="hamburger-button"
+        onClick={() => {
+          toggleNavDrawer((isOpen) => !isOpen);
+        }}
+      >
+        &#x2630;
       </div>
     </nav>
   );
